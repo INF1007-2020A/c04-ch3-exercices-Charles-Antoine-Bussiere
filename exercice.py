422ccac22e62442f7d24e0ff014e5b6c2536a9d0
Charles-Antoine-Bussiere <charles-antoine.bussiere@polymtl.ca>
2020-09-14 23:44:02 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -22,7 +22,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    angle_rads = math.degrees(a)
+    angle_rads = math.degrees(angle_rads)
     minutes = (angle_rads % 1)*60
     seconde = (minutes % 1)*60 
     return angle_rads, minutes , seconde
