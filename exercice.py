17440ded283f28018cd36b2d5c98c2dd1c4dadd8
Charles-Antoine-Bussiere <charles-antoine.bussiere@polymtl.ca>
2020-09-14 23:26:56 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -18,7 +18,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return ((a*math.pi/180) + ((angle_mins*math.pi/180)/60) + ((angle_secs*math.pi/180)/3600) )
+    return ((angle_degs*math.pi/180) + ((angle_mins*math.pi/180)/60) + ((angle_secs*math.pi/180)/3600) )
 
 
 def to_degrees(angle_rads: float) -> tuple:
